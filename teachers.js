fe807eebff32681fbddca7082d19291d4329eb7c
Jacob <jacob.fitzgerald@outlook.com>
2021-06-25 17:53:58 +0000

    add parameterized queries

--- teachers.js
+++ teachers.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 select distinct teachers.name as teacher, cohorts.name as cohort
 from teachers
 join assistance_requests on teacher_id = teachers.id
 join students on student_id = students.id
 join cohorts on cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-order by teacher;
-`)
+WHERE cohorts.name LIKE $1
+order by teacher
+limit $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
